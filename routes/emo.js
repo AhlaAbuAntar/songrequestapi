f5b264f73a75746fddd78ba3e84a4ce01368c1d7
AhlaAbuAntar <shamuonhennawi05@gmail.com>
2022-02-13 17:55:44 +0100

    asdasd

--- routes/emo.js
+++ routes/emo.js
@@ -2,8 +2,18 @@ const express = require('express');
 const emo = require('../tracklists/emo.json');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('');
+    console.log('sending emotional song request');
+})
+
 router.get('/tracks', (req, res) => {
     res.json(emo);
+    console.log('sending emotional song request');
+})
+
+router.get('/artists', (req, res) => {
+    res.json(emo.artist);
     console.log('sending emotional song request');
 })
 
