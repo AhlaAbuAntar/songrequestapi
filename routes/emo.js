64e73ddc43a02bf7726a920f253a3cef80df4760
AhlaAbuAntar <shamuonhennawi05@gmail.com>
2022-02-13 12:33:05 +0100

    asdasd

--- routes/emo.js
+++ routes/emo.js
@@ -1,53 +1,9 @@
 const express = require('express');
+const emo = require('../tracklists/emo.json');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    //Data// Content: title, artist, album, release year
-    const emoSongs = [
-        {
-            "title": "510",
-            "Artist": "Samra",
-            "Album": "Jibrail & Iblis",
-            "Release date": 2020
-        },
-        {
-            "title": "Rausch",
-            "Artist": "Samra",
-            "Album": "Rohdiamant",
-            "Release date": 2021
-        },
-        {
-            "title": "Was dann?",
-            "Artist": "Samra",
-            "Album": "Rohdiamant",
-            "Release date": 2021
-        },
-        {
-            "title": "Was dann?",
-            "Artist": "Samra",
-            "Album": "Rohdiamant",
-            "Release date": 2021
-        },
-        {
-            "title": "Jolina",
-            "Artist": "Samra",
-            "Album": "Jibrail & Iblis",
-            "Release date": 2020
-        },
-        {
-            "title": "Kennst du das?!",
-            "Artist": "Samra",
-            "Album": "Rohdiamant",
-            "Release date": 2021
-        },
-        {
-            "title": "365 Tage",
-            "Artist": "Samra",
-            "Album": "Jibrail & Iblis",
-            "Release date": 2020
-        },
-    ];
-    res.json(emoSongs);
+router.get('/tracks', (req, res) => {
+    res.json(emo);
     console.log('sending emotional song request');
 })
 
